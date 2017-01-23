16e633c4ce030c1c98050828decf36d849a3526e
Martins Donins <martins.donins@gmail.com>
2017-01-23 16:12:08 +0200

    Initial commit

---
+++ app/src/main/java/com/blogspot/stop_bullying_me/helpme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.blogspot.stop_bullying_me.helpme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
